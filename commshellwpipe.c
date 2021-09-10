f260a3fb71ddbf29a08e6f9f051ac2c7ba9f0e51
VKorastelev <niat@ngs.ru>
2021-09-10 10:56:44 +0700

    commshellwpipe with 2 fork

--- commshellwpipe.c
+++ commshellwpipe.c
@@ -33,9 +33,20 @@ void free_mem_for_exec(
         size_t *num_exec);
 
 
-int exec_prog(
-        char **args,
+int exec_progs(
+        char ***for_exec,
+        size_t *num_exec,
         bool *child);
+
+int execute_shell(
+        size_t curr_exec,
+        char ***for_exec,
+        size_t *num_exec,
+        bool *child,
+        int cond_pipes,
+        int *pipefd);
+
+int waiting_shell();
 
 
 int main()
@@ -96,13 +107,16 @@ int main()
                 break;
             }
 
+            ret = exec_progs(
+                    for_exec,
+                    &num_exec,
+                    &child);
 
-/*
-            ret = exec_prog(args, &child);
+            printf("exec_prog ret = %d\n", ret);
             if (true == child || 0 != ret)
             {
                 break;
-            }*/
+            }
 
             free_mem_for_exec(
                     for_exec, 
@@ -110,7 +124,7 @@ int main()
         }
     }
 
-    if (0  != num_exec)
+    if (0 != num_exec)// && !child)
     {
         free_mem_for_exec(
                 for_exec,
@@ -253,21 +267,203 @@ void free_mem_for_exec(
 }
 
 
-int exec_prog(
-        char **args,
-        bool *const child)
+typedef enum Pipe_conditions
+{
+    COND_NOT_PIPE = -1,
+    COND_WITH_PIPE = 1,
+} Pipe_conditions;
+
+
+void close_pipes(int pipe_in, int pipe_out)
+{
+    if (pipe_in >= 0)
+    {
+        close (pipe_in);
+    }
+    if (pipe_out >= 0)
+    {
+        close (pipe_out);
+    }
+}
+
+int exec_progs(
+        char ***for_exec,
+        size_t *num_exec,
+        bool *child)
+{
+    int ret = 0;
+    size_t i = 0;
+
+
+    *child = false;
+    int cond_pipes;
+
+    static int pipefd[2] = {0 , 0};
+
+    if (*num_exec <= 0)
+    {
+        fprintf(stderr, "No exec progs, num exec = %ld\n", *num_exec);
+        ret = EXIT_FAILURE;
+        goto finally;
+    }
+    else if (1 == *num_exec)
+    {
+        cond_pipes = COND_NOT_PIPE;
+    }
+    else
+    {
+        cond_pipes = COND_WITH_PIPE;
+    }
+
+    for (i = 0; i < *num_exec; i++)
+    {
+
+
+    if (COND_WITH_PIPE == cond_pipes && 0 == i)
+    {
+        puts("create pipe pipe(pipefd)");
+        if (-1 == pipe(pipefd))
+        {
+            perror("Error in pipe(...), function exec_progs(...)");
+            ret = -1;
+            break;
+        }
+    }
+
+
+
+        ret = execute_shell(
+                i,
+                for_exec,
+                num_exec,
+                child,
+                cond_pipes,
+                pipefd);
+
+/*
+
+        switch (cond_pipes)
+        {
+            case COND_WITHOUT_PIPES:
+            {
+                execute_shell();
+                break;
+            }
+            case COND_WITH_PIPES:
+            {
+
+                break;
+            }
+            default:
+            {
+                ret = -1;
+                break;
+            }
+        }
+*/        
+        if (0 != ret || *child)
+        {
+            break;
+        }
+    }
+
+    if (0 != ret && 0 == i)
+    {
+        goto finally;
+    }
+
+    if (!(*child))
+    {
+        puts("Before close_pipes()");
+if (COND_WITH_PIPE == cond_pipes)
+{
+    close_pipes(pipefd[0], pipefd[1]);
+}
+    printf("before waiting ret = %d\n", ret);
+    ret = waiting_shell();
+    printf("after waiting ret = %d\n", ret);
+    }
+
+ finally:
+
+    return ret;
+}
+
+typedef enum Pipe_state
+{
+    STATE_OUT_PIPE = 0,
+    STATE_TWO_PIPES,
+    STATE_IN_PIPE,
+} Pipe_state;
+
+
+int dup_pipes(int pipe_in, int pipe_out)
 {
     int ret = 0;
 
-    pid_t pid;
-    int wstatus = 0;
+    if (pipe_out > COND_NOT_PIPE)
+    {
+        if (-1 == dup2(pipe_out, 1))
+        {
+            fprintf(stderr, "Error in pipe_out dup2(%d, %d)\n", pipe_out, 1);
+            ret = -1;
+        }
+	    if (1 != pipe_out)
+        {
+            close (pipe_out);
+        }
+    }
 
+    if (pipe_in > COND_NOT_PIPE)
+    {
+        if (-1 == dup2(pipe_in, 0))
+        {
+            fprintf(stderr, "Error in pipe_in dup2(%d, %d)\n", pipe_in, 0);
+            ret = -1;
+        }
+        if (0 != pipe_in)
+        {
+            close (pipe_in);
+        }
+    }
+
+    return ret;
+}
+
+int execute_shell(
+        size_t curr_exec,
+        char ***for_exec,
+        size_t *num_exec,
+        bool *child,
+        int cond_pipes,
+        int *pipefd)
+{
+    int ret = 0;
+    pid_t pid;
+
+    *child = false;
+/*
+    static int pipefd[2] = {0 , 0};
+
+    if (COND_WITH_PIPE == cond_pipes && 0 == curr_exec)
+    {
+        puts("create pipe pipe(pipefd)");
+        if (-1 == pipe(pipefd))
+        {
+            perror("Error in pipe(...), function exec_progs(...)");
+            ret = -1;
+            goto finally;
+        }
+    }
+
+    printf("curr_exec = %ld, pipefd[0] = %d, pipefd[1] = %d\n", curr_exec, pipefd[0], pipefd[1]);
+*/
     errno = 0;
     switch (pid = fork())
     {
         case -1: // Error in fork()
         {
-            *child = false;
+            //*child = false;
 
             perror("Error in fork()");
             ret = -1;
@@ -276,9 +472,29 @@ int exec_prog(
         case 0: // Child process
         {
             *child = true;
+            
+            if (COND_WITH_PIPE == cond_pipes && 0 == curr_exec)
+            {
+                close(pipefd[0]);
+                dup_pipes(COND_NOT_PIPE, pipefd[1]);
+            }
+            else if (COND_WITH_PIPE == cond_pipes && 1 == curr_exec)
+            {
+                close(pipefd[1]);
+                dup_pipes(pipefd[0], COND_NOT_PIPE);
+            }
+            /*  
+            if (COND_WITH_PIPE == cond_pipes)
+            {
+            //close_pipes(pipe_in, pipe_out);
+            dup_pipes(pipe_in, pipe_out);
+            }
+            */
+
+            sleep(2);
 
             errno = 0;
-            if (-1 == execvp(args[0], args) && 0 != errno)
+            if (-1 == execvp(for_exec[curr_exec][0], for_exec[curr_exec]) && 0 != errno)
             {
                 ret = errno;
                 perror("Error in command");
@@ -291,29 +507,47 @@ int exec_prog(
         }
         default: // Parent process
         {
-            *child = false;
-
-            if (-1 == waitpid(pid, &wstatus, 0))
-            {
-                perror("Error in wait(...)");
-                ret = -1;
-                goto finally;
-            }
-
-            if (WIFEXITED(wstatus))
-            {
-                printf("Shell returned status: %d\n", WEXITSTATUS(wstatus));
-            }
-            else
-            {
-                printf("Shell aborted/interrupded with status: %d\n", wstatus);
-            }
-
+            //*child = false;
             break;
         }
     }
 
  finally:
+
+    return ret;
+}
+
+int waiting_shell()
+{
+    int wstatus;
+    int ret = 0;
+    pid_t pid;
+    size_t i = 1;
+
+    errno = 0;
+
+    while ((pid = waitpid(-1, &wstatus, 0)) > 0)
+    {
+    printf("in while ret = %d i = %ld\n", ret, i);
+        if (WIFEXITED(wstatus))
+        {
+            printf("Shell %ld returned status: %d\n", i, WEXITSTATUS(wstatus));
+        }
+        else
+        {
+            printf("Shell %ld aborted/interrupded with status: %d\n", i, wstatus);
+        }
+        i++;
+        errno = 0;
+    }
+
+    printf("after while  ret = %d i = %ld\n", ret, i);
+
+    if (ECHILD != errno)
+    {
+        perror("Error waitpid(...)");
+        ret = -1;
+    }
 
     return ret;
 }
